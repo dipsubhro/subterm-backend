20cc169df9c7f8c88bedf091d95a7e33f1e2d641
dipsubhro <sbhrdpsinha@gmail.com>
2026-03-12 22:47:54 +0530

    feat(collab): add join-file room handling

--- collab.js
+++ collab.js
@@ -4,6 +4,15 @@ const docs = new Map();
 
 function setupCollab(io, USER_DIR) {
   const collab = io.of("/collab");
+
+  collab.on("connection", (socket) => {
+    socket.on("join-file", (filePath) => {
+      socket.join(filePath);
+      if (!docs.has(filePath)) {
+        docs.set(filePath, new Y.Doc());
+      }
+    });
+  });
 }
 
 module.exports = { setupCollab };
